fa71c767d996affc7479d1ecf70404f738723697
Alanna Bouloy <lanihope@gmail.com>
2021-01-12 10:45:41 -0500

    readying to deploy

---
+++ build/precache-manifest.3014ea5eb19828e3faf9b42fb9d7edf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57de671ea80239e49c3d3ebc9fe7f9e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e0d9a1c07cad9bf7b7",
+    "url": "/static/css/main.c04d7450.chunk.css"
+  },
+  {
+    "revision": "de859523cffeb788ba7d",
+    "url": "/static/js/2.8f62b681.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.8f62b681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e0d9a1c07cad9bf7b7",
+    "url": "/static/js/main.6c872d33.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "0756f5a03eac92af0664167bb0f626b4",
+    "url": "/static/media/cat-and-dog.0756f5a0.jpg"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
     API_ENDPOINT: "https://mysterious-thicket-57581.herokuapp.com/api",
-    API_TOKEN: process.env.REACT_APP_API_TOKEN || 'db7b428c-4b92-11eb-ae93-0242ac130002'
+    API_TOKEN: process.env.REACT_APP_API_TOKEN
 }
+
